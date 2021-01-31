2589adc3c5f823ed3d4b8f81c8f3b9d6bfded9a2
Brian Linkletter <mail@brianlinkletter.ca>
2021-01-31 11:36:46 -0500

    Fixed packaging issues 1

--- azruntime/setup.py
+++ azruntime/setup.py
@@ -5,8 +5,9 @@ setup(
     url='https://github.com/blinklet/azure-scripts/azruntime',
     author='Brian Linkletter',
     author_email='mail@brianlinkletter.ca',
-    packages=['azruntime'],
+    packages=['.'],
     install_requires=[
+        'wheel',
         'azure-identity',
         'azure-mgmt-resource',
         'azure-mgmt-compute',
@@ -14,7 +15,7 @@ setup(
         'azure-cli-core',
         'tabulate'
     ],
-    version='0.3',
+    version='0.4',
     license='GPLv3',
     description='Print a list of all running VMs in your subscriptions. Use must be logged into Azure CLI.',
     long_description=open('README.md').read(),
